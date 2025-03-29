f0ac2ac6f56dd99e1e2d31d728f7458a12e4d8d5
Deivid Galindo <deivid.dd223@gmail.com>
2025-03-28 22:13:44 -0300

    Adicionado função se caso o valor inserido seja inexistente

--- main.js
+++ main.js
@@ -3,4 +3,23 @@
 function colorirDia(){
 // bloco de variaveis (escopo de função)
 let days = document.getElementById('day').value;
+let color = document.getElementById('color').value;
+let calendario = document.getElementById('calendario')
+
+// Bloco de validação de informações
+if(!days){
+    alert("Favor, informar um dia existente no calendário");
+}
+else{
+    if((days > 0 )&&(days < 31)){
+        let td = calendario.getElementsByTagName('td');
+        td.style.backgroundColor = color;
+    }
+    else{
+        alert("Favor informar uma data constante no calendário");
+    }
+}
+
+var elementos = document.querySelectorAll('td');
+
 }
