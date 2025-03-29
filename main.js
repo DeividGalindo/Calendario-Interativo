1f1bd0794f60d60d2e9d4aa81af8ea1961269843
Deivid Galindo <deivid.dd223@gmail.com>
2025-03-28 21:58:48 -0300

    Dado inicio ao JavaScript

---
+++ main.js
@@ -0,0 +1,6 @@
+// Codigo para tornar calendario interativo
+// Utilizando função declarativa
+function colorirDia(){
+// bloco de variaveis (escopo de função)
+let days = document.getElementById('day').value;
+}
