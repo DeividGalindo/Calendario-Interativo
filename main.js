50bfef03474a7fdfb2f730b0c7a69d5bfb1b461f
Deivid Galindo <deivid.dd223@gmail.com>
2025-04-04 21:24:54 -0300

    Adicionado as cores em java script

--- main.js
+++ main.js
@@ -22,4 +22,26 @@ else{
 
 var elementos = document.querySelectorAll('td');
 
+// Contador de cores utilizados
+var contadorAzul = 0, contadorVerde = 0, contadorRosa = 0, contadorRoxo = 0;
+for(let i = 0; i < elementos.length; i++){
+    // Obtem o estilo aplicado do elemento atual
+    var estilo = window.getComputedStyle(elementos[i]);
+    // obtem a cor de fundo atual da celula
+    var corEstilo = estilo.backgroundColor;
+
+    // verifica a cor e incrementa o contador correspondente
+    if(corEstilo == "rgb(173,216, 230" || "Lightblue"){
+        contadorAzul++;
+    }
+    else if(corEstilo == "rgb(152, 251, 152)" || corEstilo == "palegreen"){
+        contadorVerde++;
+    }
+    else if(corEstilo == "rgb(255, 182, 193)" || corEstilo == "lightPink"){
+        contadorRosa++;
+    }
+    else if(corEstilo == "rgb(106, 90, 205)" || corEstilo == "slateBlue"){
+        contadorAzul++;
+}
+
 }
